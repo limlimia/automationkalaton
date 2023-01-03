e1eaed714764ecab33dd00c55732298033a92fd6
Limia <limiakristiani@gmail.com>
2023-01-03 17:20:09 +0700

    add mobile automation

---
+++ Test Suites/Mobile testing/Login.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a6678d98-01cc-4a1e-9d6d-cb0388eed9de</testSuiteGuid>
+   <testCaseLink>
+      <guid>4e9ba26a-27f0-4d72-8ce9-f133aaecf001</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile Testing/Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>0777ff1d-af93-43ed-a6b3-ba3db374bd84</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Login</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>0777ff1d-af93-43ed-a6b3-ba3db374bd84</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>5e92953c-cb0f-409c-97f9-e0f0dd19385e</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>0777ff1d-af93-43ed-a6b3-ba3db374bd84</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>2c51b370-a74d-4060-877b-9fbe6f0adccc</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
